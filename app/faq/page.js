5ced006e71c3eafa340c96aa05ac0a22fdb572aa
Taeyoung <tseon@opuscore.net>
2026-01-10 11:22:24 +0900

    fix: modified privacy/page.js, faq/page.js: schema.prisma

---
+++ app/faq/page.js
@@ -0,0 +1,4 @@
+// app/faq/page.js 예시
+export default function FaqPage() {
+    return <div>FAQ 페이지 준비 중입니다.</div>;
+  }
---
+++ app/privacy/page.js
@@ -0,0 +1,4 @@
+// app/privacy/page.js 예시
+export default function PrivacyPage() {
+    return <div>Privacy 페이지 준비 중입니다.</div>;
+  }
